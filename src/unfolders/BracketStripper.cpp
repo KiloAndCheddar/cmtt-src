bda3e5ce3c4beff74d829213b329ccf6538399b6
KiloAndCheddar <kiloandcheddar@gmail.com>
2026-05-31 18:04:40 +0300

    Update BracketStripper.cpp basic idea testing

--- src/unfolders/BracketStripper.cpp
+++ src/unfolders/BracketStripper.cpp
@@ -1,3 +1,32 @@
+#include <string>
+#include <cstdio>
+
+// using std::string for now
+void process_file(const std::string& input) {
+    std::string buffer; 
+    bool insideBracket = false; 
+
+    for (char c : input) {
+        if (c == '[') {
+            insideBracket = true;
+            buffer.clear(); // Clear the box
+        }
+        else if (c == ']') {
+            insideBracket = false;
+            // Grab our transpiled string back
+            std::string result = handle_cmsg_construct(buffer);
+            // Write it out fast
+            printf("%s", result.c_str());
+        }
+        else if (insideBracket) {
+            buffer += c; // Grab the contents of the []'s
+        }
+        else {
+            putchar(c); // Pass normal code straight through
+        }
+    }
+}
+
 void removeBrackets(char *str) {
     char *src = str; // Pointer to read from
     char *dst = str; // Pointer to write to
