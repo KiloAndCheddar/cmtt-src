2b14d34da25e527a281903cac9c320415dc05148
KiloAndCheddar <kiloandcheddar@gmail.com>
2026-05-31 20:06:17 +0300

    Update BracketStripper.cpp

--- src/unfolders/BracketStripper.cpp
+++ src/unfolders/BracketStripper.cpp
@@ -1,31 +1,11 @@
-#include <string>
-#include <cstdio>
+//#include <stdio.h>
+//#include <string.h> // strlen() or strcpy().
+//#include <stdlib.h> // Dynamically allocating memory for strings.
+//#include <ctype.h>  // Check if characters are alphanumeric.
 
-// using std::string for now
-void process_file(const std::string& input) {
-    std::string buffer; 
-    bool insideBracket = false; 
-
-    for (char c : input) {
-        if (c == '[') {
-            insideBracket = true;
-            buffer.clear(); // Clear the box
-        }
-        else if (c == ']') {
-            insideBracket = false;
-            // Grab our transpiled string back
-            std::string result = handle_cmsg_construct(buffer);
-            // Write it out fast
-            printf("%s", result.c_str());
-        }
-        else if (insideBracket) {
-            buffer += c; // Grab the contents of the []'s
-        }
-        else {
-            putchar(c); // Pass normal code straight through
-        }
-    }
-}
+// I should NOT have slept through the cpp tutorials frfr
+// BracketStripper.cpp.cpp
+#include "BracketStripper.hpp"
 
 void removeBrackets(char *str) {
     char *src = str; // Pointer to read from
